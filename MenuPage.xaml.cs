0d49b4e0633383dba8e3f174b769eedd1fca38ac
Jacob Makota <Jacob.Makota@student.tafesa.edu.au>
2026-09-07 17:12:21 +0930

    Added menu navigation handlers

--- MenuPage.xaml.cs
+++ MenuPage.xaml.cs
@@ -1,12 +1,54 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Calculator;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Calculator
+
 {
-	public sealed partial class MenuPage : Page
-	{
-		public MenuPage()
-		{
-			this.InitializeComponent();
-		}
-	}
+
+public sealed partial class MenuPage : Page
+
+{
+
+public MenuPage()
+
+{
+this.InitializeComponent();
+}
+
+private void MathCalculator_Click(object sender, RoutedEventArgs e)
+
+{
+
+Frame.Navigate(typeof(MainPage));
+
+}
+private void MortgageCalculator_Click(object sender, RoutedEventArgs e)
+
+{
+	
+// Add mortgage navigation later
+}
+
+private void CurrencyCalculator_Click(object sender, RoutedEventArgs e)
+
+{
+	
+Frame.Navigate(typeof(CurrencyConverterPage));
+	
+}
+
+
+
+
+private void Exit_Click(object sender, RoutedEventArgs e)
+
+{
+
+Application.Current.Exit();
+	
+}
+
+}
+
 }
