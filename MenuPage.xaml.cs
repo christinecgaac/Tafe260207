32422566c3e67e64c58b691807a08fe5f0bb2f51
Jacob Makota <Jacob.Makota@student.tafesa.edu.au>
2026-09-07 17:29:03 +0930

    Added menu navigation handlers

--- MenuPage.xaml.cs
+++ MenuPage.xaml.cs
@@ -3,57 +3,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace Calculator
-
 {
-<<<<<<< HEAD
-
-public sealed partial class MenuPage : Page
-
-{
-
-public MenuPage()
-
-{
-this.InitializeComponent();
-}
-
-private void MathCalculator_Click(object sender, RoutedEventArgs e)
-
-{
-
-Frame.Navigate(typeof(MainPage));
-
-}
-private void MortgageCalculator_Click(object sender, RoutedEventArgs e)
-
-{
-	
-// Add mortgage navigation later
-}
-
-private void CurrencyCalculator_Click(object sender, RoutedEventArgs e)
-
-{
-	
-Frame.Navigate(typeof(CurrencyConverterPage));
-	
-}
-
-
-
-
-private void Exit_Click(object sender, RoutedEventArgs e)
-
-{
-
-Application.Current.Exit();
-	
-}
-
-}
-
-}
-=======
 	public sealed partial class MenuPage : Page
 	{
 		public MenuPage()
@@ -61,10 +11,25 @@ Application.Current.Exit();
 			this.InitializeComponent();
 		}
 
-		private void MortgageButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		private void MathCalculator_Click(object sender, RoutedEventArgs e)
+		{
+			Frame.Navigate(typeof(MainPage));
+		}
+
+		private void MortgageCalculator_Click(object sender, RoutedEventArgs e)
 		{
 			Frame.Navigate(typeof(MortgagePage));
 		}
+
+		private void CurrencyCalculator_Click(object sender, RoutedEventArgs e)
+		{
+			Frame.Navigate(typeof(CurrencyConverterPage));
+		}
+
+		private void Exit_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Exit();
+		}
 	}
 }
->>>>>>> remotes/origin/feature/mortgage-calculator
+
